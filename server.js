8492abfea55126f20fbcba587d1c5a396414cc6b
josedamseaux <josedamseaux@gmail.com>
2020-10-01 23:42:08 -0300

    aber5

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist','new-covid')));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'dist','new-covid','index.html'))};
+res.sendFile(path.join(__dirname,'dist','new-covid','index.html'));});
 app.listen(process.env.PORT || 8080);
