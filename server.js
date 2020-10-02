2fc6928fae1dc31cbf33a84e4ad3ec92da3228ac
josedamseaux <josedamseaux@gmail.com>
2020-10-01 23:40:25 -0300

    aber3

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/new-covid'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/new-covid/index.html'));});
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/new-covid'));
+app.use(express.static(path.join(__dirname, 'dist','new-covid')));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/new-covid/index.html'));});
+res.sendFile(path.join(__dirname,'dist','new-covid','index.html'));
 app.listen(process.env.PORT || 8080);
+
+
+//
